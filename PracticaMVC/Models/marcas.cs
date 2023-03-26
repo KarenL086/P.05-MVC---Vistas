77bea885a4997ad8baca04b40364af235ac68330
Karen <karenlm086@gmail.com>
2023-03-25 18:57:04 -0600

    Agregar archivos de proyecto.

---
+++ PracticaMVC/Models/marcas.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PracticaMVC.Models
+{
+    public class marcas
+    {
+        [Key]
+        public int id_marcas { get; set; }
+
+        [Display (Name = "Nombre marca")]
+        public string? nombre_marca { get; set; }
+
+        [Display(Name = "Estado")]
+        public string? estados { get; set; }
+    }
+}
