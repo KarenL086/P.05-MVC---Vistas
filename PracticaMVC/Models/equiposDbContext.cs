b7acb132f09c5066e69df7ea9b4d6fb41b6c930e
Karen <karenlm086@gmail.com>
2023-03-28 22:28:25 -0600

    MVC CRUDs

--- PracticaMVC/Models/equiposDbContext.cs
+++ PracticaMVC/Models/equiposDbContext.cs
@@ -8,5 +8,11 @@ namespace PracticaMVC.Models
         {
         }
         public DbSet<marcas> marcas { get; set; }
+        public DbSet<carreras> carreras { get; set; }   
+        public DbSet<usuarios> usuarios { get; set; }
+        public DbSet<tipo_equipo> tipo_equipo { get; set; }
+        public DbSet<facultades> facultades { get; set;}
+        public DbSet<estados_reserva> estados_reserva { get; set; }
+        public DbSet<estados_equipo> estados_equipo { get; set; }
     }
 }
